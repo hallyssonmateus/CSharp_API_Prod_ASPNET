55689ed78531a3987319dc8db2e8038025a47135
hallyssonmateus <hallysson_gato@hotmail.com>
2024-08-27 18:45:08 -0300

    feat: Criando arquivos/pastas API e add pacotes EF

---
+++ Models/Produto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharp_API_Prod_ASPNET.Models
+{
+    public class Produto
+    {
+        
+    }
+}
